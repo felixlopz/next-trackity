74a282d6f7abd6ebb98ddabff7f582c6761fcb90
Felix Lopez <felixlopzd@gmail.com>
2024-08-16 12:21:27 -0400

    feat: api of transactions

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,6 +4,7 @@ import { HTTPException } from "hono/http-exception";
 
 import accounts from "./accounts";
 import categories from "./categories";
+import transactions from "./transactions";
 
 export const runtime = "edge";
 
@@ -19,7 +20,8 @@ const app = new Hono().basePath("/api");
 
 const routes = app
   .route("/accounts", accounts)
-  .route("/categories", categories);
+  .route("/categories", categories)
+  .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
