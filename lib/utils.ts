4b8dfc49e2c99bff6bcb01ad913aec9347ee8165
Felix Lopez <felixlopzd@gmail.com>
2024-08-19 13:03:54 -0400

    feat: summary api

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { twMerge } from "tailwind-merge";
 import { type ClassValue, clsx } from "clsx";
+import { eachDayOfInterval, isSameDay } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -23,4 +24,32 @@ export function formatCurrency(value: number) {
 
 export function isValidDate(date: Date): boolean {
   return !isNaN(date.getTime());
+}
+
+export function calculatePercentangeChange(current: number, previous: number) {
+  if (previous === 0) {
+    return previous === current ? 0 : 100;
+  }
+
+  return ((current - previous) / previous) * 100;
+}
+
+export function fillMissingDays(
+  activeDays: { date: Date; income: number; expenses: number }[],
+  startDate: Date,
+  endDate: Date
+) {
+  if (activeDays.length === 0) {
+    return [];
+  }
+
+  const allDays = eachDayOfInterval({ start: startDate, end: endDate });
+
+  const transactionsByDate = allDays.map((day) => {
+    const found = activeDays.find((d) => isSameDay(d.date, day));
+
+    return found != null ? found : { date: day, income: 0, expenses: 0 };
+  });
+
+  return transactionsByDate;
 }
